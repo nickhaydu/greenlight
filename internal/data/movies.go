5bf0a80a093230302ecebe45e85bcf8e3d3a2506
nickhaydu <nickhaydu2002@gmail.com>
2026-02-25 22:28:00 -0400

    added delete route and patch updates plus race condition handling through optimisitic locking

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -84,14 +84,38 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 func (m MovieModel) Update(movie *Movie) error {
 	query := `UPDATE movies
 	SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
-	WHERE id = $5
+	WHERE id = $5 AND version = $6
 	RETURNING version`
 
-	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.ID}
+	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.ID, movie.Version}
 
-	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
 }
 
 func (m MovieModel) Delete(id int64) error {
+	query := `DELETE FROM movies WHERE id = $1`
+	result, err := m.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffect, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffect == 0 {
+		return ErrRecordNotFound
+	}
+
 	return nil
 }
